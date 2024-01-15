c06f9978fd530f78ca46aa3620fd8e281ecba999
Minalu Mesele <besratmesele@gmail.com>
2024-01-15 22:27:02 +0300

    one more activity and a service added to the application.

---
+++ app/src/main/java/com/minun/freecodecamp/ListActivity.java
@@ -0,0 +1,24 @@
+package com.minun.freecodecamp;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        Button btnBack = findViewById(R.id.buttonBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
